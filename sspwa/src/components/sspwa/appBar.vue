c2adbc2cde7ba7835a8ce409cb870d468dd94e2a
Mike Laeta <mike@laetadevelopment.com>
2022-04-27 13:05:30 -0600

    t1.1.1.1
    add content and basic functionality for project

---
+++ sspwa/src/components/sspwa/appBar.vue
@@ -0,0 +1,13 @@
+<template>
+  <div id="appBar">
+    <button>Sponsor</button>
+    <button>Discuss</button>
+    <button>Download</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'appBar'
+}
+</script>
